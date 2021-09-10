33c9b0ff5aac466fdd5f4f1395dbc0717ad94b3b
Andenor <rowebot98@gmail.com>
2021-09-09 18:50:53 -0600

    Add remaining token types to enum

--- Token.h
+++ Token.h
@@ -5,7 +5,21 @@
 enum class TokenType {
     COLON,
     COLON_DASH,
-    // TODO: add the other types of tokens
+    COMMA,
+    PERIOD,
+    Q_MARK,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    MULTIPLY,
+    ADD,
+    SCHEMES,
+    FACTS,
+    RULES,
+    QUERIES,
+    ID,
+    STRING,
+    COMMENT,
+    END_OF_FILE,
     UNDEFINED
 };
 
@@ -19,7 +33,64 @@ private:
 public:
     Token(TokenType type, std::string description, int line);
     std::string ToString() {
-        return "(type, " + description + ", " + std::to_string(line) + ")\n";
+        std::string typeString;
+        switch (type) {
+            case TokenType::COLON:
+                typeString = "COLON";
+                break;
+            case TokenType::COLON_DASH:
+                typeString = "COLON_DASH";
+                break;
+            case TokenType::COMMA:
+                typeString = "COMMA";
+                break;
+            case TokenType::PERIOD:
+                typeString = "PERIOD";
+                break;
+            case TokenType::Q_MARK:
+                typeString = "Q_MARK";
+                break;
+            case TokenType::LEFT_PAREN:
+                typeString = "LEFT_PAREN";
+                break;
+            case TokenType::RIGHT_PAREN:
+                typeString = "RIGHT_PAREN";
+                break;
+            case TokenType::MULTIPLY:
+                typeString = "MULTIPLY";
+                break;
+            case TokenType::ADD:
+                typeString = "ADD";
+                break;
+            case TokenType::SCHEMES:
+                typeString = "SCHEMES";
+                break;
+            case TokenType::FACTS:
+                typeString = "FACTS";
+                break;
+            case TokenType::RULES:
+                typeString = "RULES";
+                break;
+            case TokenType::QUERIES:
+                typeString = "QUERIES";
+                break;
+            case TokenType::ID:
+                typeString = "ID";
+                break;
+            case TokenType::STRING:
+                typeString = "STRING";
+                break;
+            case TokenType::COMMENT:
+                typeString = "COMMENT";
+                break;
+            case TokenType::END_OF_FILE:
+                typeString = "EOF";
+                break;
+            case TokenType::UNDEFINED:
+                typeString = "UNDEFINED";
+                break;
+        }
+        return "(" + typeString +", \"" + description + "\", " + std::to_string(line) + ")\n";
     }
 
     // TODO: add other needed methods
