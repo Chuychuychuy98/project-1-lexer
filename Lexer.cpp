d6e01fddfff9116be3ffb4402e9db1a137fa35fe
Andenor <rowebot98@gmail.com>
2021-09-09 21:36:04 -0600

    Implement remaining single-character automata

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,6 +1,13 @@
 #include "Lexer.h"
 #include "ColonAutomaton.h"
 #include "ColonDashAutomaton.h"
+#include "CommaAutomaton.h"
+#include "PeriodAutomaton.h"
+#include "QMarkAutomaton.h"
+#include "LeftParenAutomaton.h"
+#include "RightParenAutomaton.h"
+#include "MultiplyAutomaton.h"
+#include "AddAutomaton.h"
 
 Lexer::Lexer() {
     CreateAutomata();
@@ -19,6 +26,13 @@ Lexer::~Lexer() {
 void Lexer::CreateAutomata() {
     automata.push_back(new ColonAutomaton());
     automata.push_back(new ColonDashAutomaton());
+    automata.push_back(new CommaAutomaton());
+    automata.push_back(new PeriodAutomaton());
+    automata.push_back(new QMarkAutomaton());
+    automata.push_back(new LeftParenAutomaton());
+    automata.push_back(new RightParenAutomaton());
+    automata.push_back(new MultiplyAutomaton());
+    automata.push_back(new AddAutomaton());
     // TODO: Add the other needed automata here
 }
 
